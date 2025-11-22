b8b914075473bfcff6afdc1ce97f446b74e6b92b
qiross <lucasqreis@poli.ufrj.br>
2025-11-22 19:20:13 -0300

    Terraform format

--- terraform/main.tf
+++ terraform/main.tf
@@ -527,7 +527,7 @@ resource "aws_sns_topic" "alerts" {
 resource "aws_sns_topic_subscription" "email_alerts" {
   topic_arn = aws_sns_topic.alerts.arn
   protocol  = "email"
-  endpoint  = var.alert_email  # Você vai adicionar esta variável
+  endpoint  = var.alert_email
 }
 
 # 8. CloudWatch Dashboard para monitoramento visual
