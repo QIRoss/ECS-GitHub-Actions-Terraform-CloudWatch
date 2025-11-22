be1d2de5719c5b8e1300cef285880dc97fd15aef
qiross <lucasqreis@poli.ufrj.br>
2025-11-22 19:46:51 -0300

    Terraform format

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "eloquent-ai-app-tfstate"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
+    bucket = "eloquent-ai-app-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
     # profile        = "qiross"
     encrypt        = true
     dynamodb_table = "eloquent-ai-app-tfstate-lock"
