772c6be3d028ce782705ff59f01d193bd7431550
qiross <lucasqreis@poli.ufrj.br>
2025-11-22 21:42:23 -0300

    First Deploy update

--- first_deploy.sh
+++ first_deploy.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 set -e
 
+echo "=== Phase 0: Setting up Terraform Backend ==="
+
+# Create S3 bucket
+aws s3 mb s3://eloquent-ai-app-tfstate --region us-east-1
+
+# Enable S3 bucket versioning
+aws s3api put-bucket-versioning \
+    --bucket eloquent-ai-app-tfstate \
+    --versioning-configuration Status=Enabled \
+
+# Create DynamoDB table for Terraform state locking
+aws dynamodb create-table \
+    --table-name eloquent-ai-app-tfstate-lock \
+    --attribute-definitions AttributeName=LockID,AttributeType=S \
+    --key-schema AttributeName=LockID,KeyType=HASH \
+    --billing-mode PAY_PER_REQUEST \
+    --region us-east-1 \
+
+
+# Verify S3 bucket
+aws s3 ls s3://eloquent-ai-app-tfstate/
+
 echo "=== Phase 1: Creating ECR ==="
 cd terraform
+terraform state list || echo "No existing state found, proceeding..."
 terraform init
 terraform apply -target=aws_ecr_repository.app -auto-approve
 
@@ -15,31 +38,6 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker build -t $ECR_URL:latest .
 docker push $ECR_URL:latest
 
-echo "=== Fase 3: Criando Resto da Infra ==="
+echo "=== Phase 3: Complete Terraform Creation ==="
 cd terraform
 terraform apply -auto-approve
-
-echo "=== Deploy Complete! ==="
-ALB_URL=$(terraform output -raw alb_dns_name)
-echo "URL: http://$ALB_URL"
-
-# Create S3 bucket
-# aws s3 mb s3://eloquent-ai-app-tfstate --region us-east-1
-
-# # Enable S3 bucket versioning
-# aws s3api put-bucket-versioning \
-#     --bucket eloquent-ai-app-tfstate \
-#     --versioning-configuration Status=Enabled \
-
-# # Create DynamoDB table for Terraform state locking
-# aws dynamodb create-table \
-#     --table-name eloquent-ai-app-tfstate-lock \
-#     --attribute-definitions AttributeName=LockID,AttributeType=S \
-#     --key-schema AttributeName=LockID,KeyType=HASH \
-#     --billing-mode PAY_PER_REQUEST \
-#     --region us-east-1 \
-
-# terraform state list
-
-# # Verify S3 bucket
-# aws s3 ls s3://eloquent-ai-app-tfstate/
